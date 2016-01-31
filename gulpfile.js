757ca5fdb4eff39093b2d23c1ad31a0d714cdbc9
Clark Malmgren <cmalmgren@gmail.com>
2016-01-30 23:42:47 -0600

    Resolves #5 - Add support for generating coverage and sending to coveralls.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@
       jspm = require('gulp-jspm'),
       babel = require('gulp-babel'),
       mocha = require('gulp-mocha'),
+      istanbul = require('gulp-istanbul'),
+      coveralls = require('gulp-coveralls'),
       sourcemaps = require('gulp-sourcemaps');
 
   /* Require this to ensure that mocha runs using es6 */
@@ -27,9 +29,33 @@
       .pipe(gulp.dest('dist'));
   });
 
-  gulp.task('test', function() {
-    return gulp.src('src/*.js')
-      .pipe(mocha({reporter: 'nyan'}));
+  gulp.task('test:build', function() {
+    return gulp.src(['src/*.js', 'src/*.js'])
+      .pipe(sourcemaps.init())
+        .pipe(babel())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('build/es5'));
+
+  });
+
+  gulp.task('test:instrument', ['test:build'], function() {
+    return gulp.src(['build/es5/ezdocker.js', 'build/es5/tar-utils.js'])
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire());
+  });
+
+  gulp.task('test', ['test:instrument'], function() {
+    return gulp.src('build/es5/*.js')
+      .pipe(mocha())
+      .pipe(istanbul.writeReports({
+        dir: './build/coverage',
+        reporters: ['lcovonly', 'html', 'text-summary']
+      }));
+  });
+
+  gulp.task('ci', [ 'test' ], function() {
+    return gulp.src('build/coverage/lcov.info')
+      .pipe(coveralls());
   });
 
 })();
