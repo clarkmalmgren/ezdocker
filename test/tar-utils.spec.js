e7459c9fb9d85410a3855c905a7f3f9a4afc9472
Clark Malmgren <cmalmgren@gmail.com>
2016-02-03 23:28:47 -0500

    Test coverage for tar-utils.

--- test/tar-utils.spec.js
+++ test/tar-utils.spec.js
@@ -3,12 +3,18 @@ import TarUtils from '../dist/tar-utils';
 
 describe('TarUtils', () => {
 
+  afterEach(() => {
+    if (Math.random.restore) {
+      Math.random.restore();
+    }
+  });
+
   describe('temporary folder generation', () => {
 
     it('should delegate to os for finding temporary directory root', () => {
       // given:
-      let os = { tmpdir : () => {} };
-      sinon.stub(os, 'tmpdir').returns('/hawaii');
+      let os = { tmpdir: sinon.stub().returns('/hawaii') };
+      sinon.stub(Math, 'random').returns(1);
 
       // when:
       let tarutils = new TarUtils(undefined, os);
@@ -16,7 +22,7 @@ describe('TarUtils', () => {
 
       // then:
       os.tmpdir.should.have.been.called;
-      temp.should.startWith('/hawaii/tar-utils__');
+      temp.should.equals('/hawaii/tar-utils__10000');
     });
 
   });
@@ -25,7 +31,7 @@ describe('TarUtils', () => {
 
     it('should register for cleanup on process exit', () => {
       // given:
-      let process = { on: sinon.spy() };
+      let process = {on: sinon.spy()};
 
       // when:
       let tarutils = new TarUtils(undefined, undefined, undefined, process);
@@ -37,7 +43,7 @@ describe('TarUtils', () => {
 
     it('cleanup should happen exactly once', () => {
       // given:
-      let process = { on: sinon.spy() };
+      let process = {on: sinon.spy()};
       let del = sinon.spy();
 
       // when:
@@ -61,6 +67,48 @@ describe('TarUtils', () => {
 
 
   });
+
+  describe('all', () => {
+    it('should create a stream', () => {
+      // given:
+      let os = { tmpdir: sinon.stub().returns('/hawaii') };
+      sinon.stub(Math, 'random').returns(1);
+
+      let process = {on: sinon.spy()};
+      let del = sinon.spy();
+
+      let extract = { on: sinon.stub() };
+
+      let copyStream = { pipe: sinon.spy() };
+
+      let tar = {
+        extract: sinon.stub(),
+        pack: sinon.stub()
+      };
+
+      tar.extract.withArgs('/hawaii/tar-utils__10000/.').returns(extract);
+      extract.on.callsArg(1);
+      tar.pack.withArgs('/chicago').returns(copyStream);
+
+      tar.pack.withArgs('/hawaii/tar-utils__10000').returns('SUCCESS!');
+
+      // when:
+      let tarutils = new TarUtils(del, os, tar, process);
+      let promise = tarutils.all({'/chicago':'.'});
+
+      // then:
+      return promise.should.eventually.equals('SUCCESS!')
+        .then(() => {
+          tar.extract.withArgs('/hawaii/tar-utils__10000/.').should.be.called;
+          tar.pack.withArgs('/chicago').should.be.called;
+          extract.on.should.be.called;
+          copyStream.pipe.should.be.called;
+          tar.pack.withArgs('/hawaii/tar-utils__10000').should.be.called;
+        });
+
+    })
+  });
+
 
 });
 
--- test/test_dependencies.js
+++ test/test_dependencies.js
@@ -3,9 +3,11 @@ import sinon from 'sinon';
 import chai from 'chai';
 import chaiString from 'chai-string';
 import sinonChai from 'sinon-chai';
+import chaiAsPromised from 'chai-as-promised';
 
 chai.use(chaiString);
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 
 chai.should();
 
