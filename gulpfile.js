864d9c652f4fd310b332804fc9658ef77f4c834a
Clark Malmgren <cmalmgren@gmail.com>
2016-02-03 21:49:13 -0500

    Add the ability to generate ESDocs. (Closes #7)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
     gulp = require('gulp'),
     jspm = require('gulp-jspm'),
     babel = require('gulp-babel'),
+    esdoc = require('gulp-esdoc'),
     mocha = require('gulp-mocha'),
     istanbul = require('gulp-istanbul'),
     coveralls = require('gulp-coveralls'),
@@ -29,13 +30,17 @@
       .pipe(gulp.dest('dist'));
   });
 
+  gulp.task('esdoc', function() {
+    return gulp.src(['src'])
+      .pipe(esdoc({ destination: './build/esdoc' }));
+  });
+
   gulp.task('test:build', function() {
     return gulp.src(['test/*.js'])
       .pipe(sourcemaps.init())
       .pipe(babel())
       .pipe(sourcemaps.write())
       .pipe(gulp.dest('dist_test'));
-
   });
 
   gulp.task('test:instrument', ['build', 'test:build'], function() {
