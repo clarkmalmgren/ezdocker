50a0dc14c4f0b44eed25b9f7a7373937f88c1dcf
Clark Malmgren <cmalmgren@gmail.com>
2016-02-05 16:43:34 -0600

    Add more unit testing

--- test/ezdocker.spec.js
+++ test/ezdocker.spec.js
@@ -82,6 +82,13 @@ describe('ImagePusher', () => {
 
 describe('EZDocker', () => {
 
+  beforeEach(() => {
+    if (Log._log.restore) {
+      Log._log.restore();
+    }
+    sinon.stub(Log, '_log');
+  });
+
   it('can be newed', () => {
     let ez = new EZDocker();
     ez.should.exist;
@@ -112,6 +119,157 @@ describe('EZDocker', () => {
     builder.should.be.an.instanceOf(ImageBuilder);
     remover.should.be.an.instanceOf(ImageRemover);
     pusher.should.be.an.instanceOf(ImagePusher);
+  });
+
+  describe('image building', () => {
+
+    it('should delegate and handle success on end', () => {
+      // given:
+      let stream = "hey, check me out, I am a stream";
+      let response = {
+        pipe: sinon.spy(),
+        on: sinon.stub().withArgs('end').callsArg(1)
+      };
+      let tarUtils = {
+        all: sinon.stub().withArgs('mappy').resolves(stream)
+      };
+      let docker = {
+        buildImage: sinon.stub().withArgs(stream, {t: 'a'}).callsArgWith(2, undefined, response)
+      };
+      let builder = {
+        _paths: 'mappy',
+        getFullName: sinon.stub().returns('a')
+      };
+
+      // when:
+      let ez = new EZDocker(undefined, docker, tarUtils);
+      let promise = ez._buildImage(builder);
+
+      // then:
+      return promise.should.eventually.be.fulfilled
+        .then(() => {
+          response.pipe.should.have.been.calledWith(stream_parser);
+        });
+    });
+
+    it('should delegate failure on failure to build the image', () => {
+      // given:
+      let stream = "hey, check me out, I am a stream";
+      let response = {
+        pipe: sinon.spy(),
+        on: sinon.spy()
+      };
+      let tarUtils = {
+        all: sinon.stub().withArgs('mappy').resolves(stream)
+      };
+      let docker = {
+        buildImage: sinon.stub().withArgs(stream, {t: 'a'}).callsArgWith(2, new Error('failz'))
+      };
+      let builder = {
+        _paths: 'mappy',
+        getFullName: sinon.stub().returns('a')
+      };
+
+      // when:
+      let ez = new EZDocker(undefined, docker, tarUtils);
+      let promise = ez._buildImage(builder);
+
+      // then:
+      return promise.should.eventually.be.rejectedWith(Error, 'failz')
+        .then(() => {
+          response.pipe.should.have.not.have.been.called
+          response.on.should.have.not.have.been.called
+        });
+    });
+
+    it('should delegate failure on creating tar stream', () => {
+      // given:
+      let tarUtils = {
+        all: sinon.stub().withArgs('mappy').rejects(new Error('oops'))
+      };
+      let docker = {};
+      let builder = { _paths: 'mappy' };
+
+      // when:
+      let ez = new EZDocker(undefined, docker, tarUtils);
+      let promise = ez._buildImage(builder);
+
+      // then:
+      return promise.should.eventually.be.rejectedWith(Error, 'oops')
+    });
+
+  });
+
+
+
+  describe('removing all images', () => {
+
+    it('should simply tie listing to removing', () => {
+      // given:
+      let images = [
+        { Id: 'a' },
+        { Id: 'b' }
+      ];
+      let remover = new ImageRemover();
+      sinon.stub(remover, 'getShortName').returns('shorty');
+
+      let ez = new EZDocker(undefined, {});
+      sinon.stub(ez, '_listImages')
+        .withArgs('shorty')
+        .resolves(images);
+
+      ez.removeImage = sinon.spy();
+
+      // when:
+      let promise = ez._removeImages(remover);
+
+      // then:
+      return promise.should.eventually.be.fulfilled
+        .then(() => {
+          ez.removeImage.should.have.been.calledTwice;
+          ez.removeImage.should.have.been.calledWith('a');
+          ez.removeImage.should.have.been.calledWith('b');
+        });
+    });
+
+  });
+
+  describe('listing images', () => {
+
+    it('should succeed as a promise', () => {
+      // given:
+      let images = 'my list of dudes';
+
+      let docker = {
+        listImages: sinon.stub()
+          .withArgs({filter: 'dude'})
+          .callsArgWith(1, undefined, images)
+      };
+
+      // when:
+      let ez = new EZDocker(undefined, docker);
+      let promise = ez._listImages('dude');
+
+      // then:
+      return promise.should.eventually.be.fulfilled.and.equal('my list of dudes');
+    });
+
+    it('should reject as a promise', () => {
+      // given:
+      let docker = {
+        listImages: sinon.stub()
+          .withArgs({filter: 'dude'})
+          .callsArgWith(1, new Error('blah'))
+      };
+
+      // when:
+      let ez = new EZDocker(undefined, docker);
+      let promise = ez._listImages('dude');
+
+      // then:
+      promise.should.eventually.be.rejectedWith(Error, 'blah');
+    });
+
   });
 
 });
--- test/test_dependencies.js
+++ test/test_dependencies.js
@@ -4,6 +4,7 @@ import chai from 'chai';
 import chaiString from 'chai-string';
 import sinonChai from 'sinon-chai';
 import chaiAsPromised from 'chai-as-promised';
+import 'sinon-as-promised';
 
 chai.use(chaiString);
 chai.use(sinonChai);
